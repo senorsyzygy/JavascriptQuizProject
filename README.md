6a5c6491c8e37931c8ffa4b5b94687c17960f8e3
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2021-11-10 12:44:15 +0000

    added images, base quiz structure, stylesheet and index

---
+++ README.md
@@ -0,0 +1,2 @@
+Javascript quiz project for The Developer Academy
+Quiz topic chosen being titles for Clint Eastwood films
---
+++ quizCode.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+  document.getElementById('question1').style.display = 'none';
+  document.getElementById('question2').style.display = 'none';
+  document.getElementById('question3').style.display = 'none';
+  document.getElementById('question4').style.display = 'none';
+  document.getElementById('question5').style.display = 'none';
+  document.getElementById('question6').style.display = 'none';
+  document.getElementById('question7').style.display = 'none';
+  document.getElementById('results').style.display = 'none'
+}
+function quizIntro() {
+  document.getElementById("intro").style.display = "inline-block";
+  document.getElementById("question1").style.display = "none";
+  document.getElementById("results").style.display = "none";
+}
+function question1() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("question1").style.display = "inline-block";
+    document.getElementById("question2").style.display = "none";
+  }
+  function question2() {
+    document.getElementById("question1").style.display = "none";
+    document.getElementById("question2").style.display = "inline-block";
+    document.getElementById("question3").style.display = "none";
+  }
+  function question3() {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("question3").style.display = "inline-block";
+    document.getElementById("question4").style.display = "none";
+  }
+  function question4() {
+    document.getElementById("question3").style.display = "none";
+    document.getElementById("question4").style.display = "inline-block";
+    document.getElementById("question5").style.display = "none";
+  }
+  function question5() {
+    document.getElementById("question4").style.display = "none";
+    document.getElementById("question5").style.display = "inline-block";
+    document.getElementById("question6").style.display = "none";
+  }
+  function question6() {
+    document.getElementById("question5").style.display = "none";
+    document.getElementById("question6").style.display = "inline-block";
+    document.getElementById("question7").style.display = "none";
+  }
+  function question7() {
+    document.getElementById("question6").style.display = "none";
+    document.getElementById("question7").style.display = "inline-block";
+    document.getElementById("results").style.display = "none";
+  }
+  function results()  {
+    document.getElementById("question7").style.display = "none";
+    document.getElementById("results").style.display = "inline-block";
+  }
