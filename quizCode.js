27793b715f9e7d6c186f734f914c08f097aae652
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2021-11-10 16:35:14 +0000

    updated js and onclick to match

--- quizCode.js
+++ quizCode.js
@@ -1,3 +1,4 @@
+//Hides all Quiz elements besides Intro
 window.onload = function() {
   document.getElementById('question1').style.display = 'none';
   document.getElementById('question2').style.display = 'none';
@@ -6,50 +7,15 @@ window.onload = function() {
   document.getElementById('question5').style.display = 'none';
   document.getElementById('question6').style.display = 'none';
   document.getElementById('question7').style.display = 'none';
-  document.getElementById('results').style.display = 'none'
+  document.getElementById('quizResults').style.display = 'none'
 }
-function quizIntro() {
-  document.getElementById("intro").style.display = "inline-block";
-  document.getElementById("question1").style.display = "none";
-  document.getElementById("results").style.display = "none";
+//Goes to the next question
+function nextQuestion(prev, next){
+  document.getElementById(prev).style.display = "none"
+  document.getElementById(next).style.display = "inline-block"
 }
-function question1() {
-  document.getElementById("intro").style.display = "none";
-  document.getElementById("question1").style.display = "inline-block";
-  document.getElementById("question2").style.display = "none";
+//Goes to the previous question
+function prevQuestion(current, prev){
+  document.getElementById(current).style.display = "none"
+  document.getElementById(prev).style.display = "inline-block"
 }
-function question2() {
-  document.getElementById("question1").style.display = "none";
-  document.getElementById("question2").style.display = "inline-block";
-  document.getElementById("question3").style.display = "none";
-}
-function question3() {
-  document.getElementById("question2").style.display = "none";
-  document.getElementById("question3").style.display = "inline-block";
-  document.getElementById("question4").style.display = "none";
-}
-function question4() {
-  document.getElementById("question3").style.display = "none";
-  document.getElementById("question4").style.display = "inline-block";
-  document.getElementById("question5").style.display = "none";
-}
-function question5() {
-  document.getElementById("question4").style.display = "none";
-  document.getElementById("question5").style.display = "inline-block";
-  document.getElementById("question6").style.display = "none";
-}
-function question6() {
-  document.getElementById("question5").style.display = "none";
-  document.getElementById("question6").style.display = "inline-block";
-  document.getElementById("question7").style.display = "none";
-}
-function question7() {
-  document.getElementById("question6").style.display = "none";
-  document.getElementById("question7").style.display = "inline-block";
-  document.getElementById("results").style.display = "none";
-}
-function results()  {
-  document.getElementById("question7").style.display = "none";
-  document.getElementById("results").style.display = "inline-block";
-}
-
