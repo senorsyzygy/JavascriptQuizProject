5db7b9dbb2e5ec4735f8ffa9194e527c7be333d7
senorsyzygy <45519112+senorsyzygy@users.noreply.github.com>
2021-11-11 09:35:13 +0000

    added calculation but its not working as of yet

--- quizCode.js
+++ quizCode.js
@@ -10,12 +10,89 @@ window.onload = function() {
   document.getElementById('quizResults').style.display = 'none'
 }
 //Goes to the next question
-function nextQuestion(prev, next){
-  document.getElementById(prev).style.display = "none"
+function nextQuestion(current, next){
+  document.getElementById(current).style.display = "none"
   document.getElementById(next).style.display = "inline-block"
 }
 //Goes to the previous question
 function prevQuestion(current, prev){
   document.getElementById(current).style.display = "none"
   document.getElementById(prev).style.display = "inline-block"
+}
+//calculates results and displays message based on differing score
+function calculatingResults(){
+  const radios1 = document.querySelectorAll('input[name="quest1"]');
+  let q1Value;
+  for (const rb of radios1) {
+      if (rb.checked) {
+          q1Value = rb.value;
+          break;
+      }
+  }
+  
+  const radios2 = document.querySelectorAll('input[name="quest2"]');
+  let q2Value;
+  for (const rb of radios2) {
+      if (rb.checked) {
+          q2Value = rb.value;
+          break;
+      }
+  }
+  const radios3 = document.querySelectorAll('input[name="quest3"]');
+  let q3Value;
+  for (const rb of radios3) {
+      if (rb.checked) {
+          q3Value = rb.value;
+          break;
+      }
+  }
+  
+  const radios4 = document.querySelectorAll('input[name="quest4"]');
+  let q4Value;
+  for (const rb of radios4) {
+      if (rb.checked) {
+          q4Value = rb.value;
+          break;
+      }
+  }
+  const radios5 = document.querySelectorAll('input[name="quest5"]');
+  let q5Value;
+  for (const rb of radios5) {
+      if (rb.checked) {
+          q5Value = rb.value;
+          break;
+      }
+  }
+  
+  const radios6 = document.querySelectorAll('input[name="quest6"]');
+  let q6Value;
+  for (const rb of radios6) {
+      if (rb.checked) {
+          q6Value = rb.value;
+          break;
+      }
+  }
+  const radios7 = document.querySelectorAll('input[name="quest7"]');
+  let q7Value;
+  for (const rb of radios7) {
+      if (rb.checked) {
+          q7Value = rb.value;
+          break;
+      }
+  }
+  let total = Number(q1Value) + Number(q2Value) + Number(q3Value) + Number(q4Value) + Number(q5Value) + Number(q6Value) + Number(q7Value)
+  console.log(total)
+  result = total 
+  if (result = 21){
+    msg = "You have gotten everything correct, well done!"
+  } else if (result = 0){
+    msg = "Somehow you managed to get everything wrong, well done!"
+  } else if (7 > result > 0) {
+    msg = "You got some close, try again and hope for a better score!"
+  } else if (21 > result >= 7) {
+    msg = "You're very close to getting them all correct, try again and see if you can get them all correct!"
+  }
+
+  document.getElementById("result").innerHTML = msg
+  console.log(msg)
 }
